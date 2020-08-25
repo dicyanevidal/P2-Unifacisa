bb9186ec3e56c25d100e07275e79dcc5b519292f
dicyane vidal <dicyanevidal@hotmail.com>
2020-08-25 12:06:45 -0300

    uri1021

--- P2-Unifacisa/src/uri1021.java
+++ P2-Unifacisa/src/uri1021.java
@@ -15,41 +15,29 @@ public class uri1021 {
 		
 		nota_100 = (int) (valor / 100);
 		valor %= 100.0;
-		
 		nota_50 = (int) (valor / 50);
 		valor %= 50.0;
-		
 		nota_20 = (int) (valor / 20);
 		valor %= 20.0;
-		
 		nota_10 = (int) (valor / 10);
 		valor %= 10.0;
-		
 		nota_5 = (int) (valor / 5);
 		valor %= 5.0;
-		
 		nota_2 = (int) (valor / 2);
 		valor %= 2.0;
-		
-		
 		
 		valor *= 100;
 		
 		moeda_1 = (int) (valor / 100);
 		valor %= 100.0;
-		
 		moeda_050 = (int) (valor / 50);
 		valor %= 50.0;
-		
 		moeda_025 = (int) (valor / 25);
 		valor %= 25.0;
-		
 		moeda_010 = (int) (valor / 10);
 		valor %= 10.0;
-		
 		moeda_005 = (int) (valor / 5);
 		valor %= 5.0;
-		
 		moeda_001 = (int) (valor / 1);
 		valor %= 1.0;
 		
@@ -61,7 +49,6 @@ public class uri1021 {
 		System.out.printf("%n%.0f nota(s) de R$ 10.00", nota_10);
 		System.out.printf("%n%.0f nota(s) de R$ 5.00", nota_5);
 		System.out.printf("%n%.0f nota(s) de R$ 2.00", nota_2);
-		
 		
 		System.out.printf("%nMOEDAS:");
 		System.out.printf("%n%.0f moeda(s) de R$ 1.00", moeda_1);
